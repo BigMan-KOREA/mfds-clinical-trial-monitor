28eab5fb0824eaddca8fae764be8d86b44374765
BigMan-KOREA <daeink@huinno.com>
2025-05-23 15:45:48 +0900

    Update mfds_monitor.py

--- mfds_monitor.py
+++ mfds_monitor.py
@@ -27,10 +27,18 @@ def setup_driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
     
-    driver = webdriver.Chrome(options=chrome_options)
+    # Chromium 경로 명시
+    chrome_options.binary_location = '/usr/bin/chromium-browser'
+    
+    from selenium.webdriver.chrome.service import Service
+    service = Service('/usr/lib/chromium-browser/chromedriver')
+    
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
-
+    
 def crawl_recent_pages(pages_to_check=5):
     """최근 N페이지 크롤링"""
     driver = setup_driver()
